e6082b79d42d1180aede86c81f57e800f3a1a63c
AndersenDanmark <andrewyan7217@gmail.com>
2018-05-14 15:07:37 -0600

    added jawsmysql to heroku

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ print("Hello World! Python")
 
 from flask import Flask
 from flask import send_from_directory
+from flask import request
+import json
 
 app=Flask(__name__,static_url_path='',static_folder='web/')
 
 print('Starting Flask')
 
-@app.route('/')
+@app.route('/add')
+def add():
+    x= int(request.args.get('x'))
+    y = int(request.args.get('y'))
+    z=x+y
+    ret = json.dumps(z)
+    return ret
 
+@app.route('/')
 def home():
 
     print('Flask')
@@ -18,3 +27,5 @@ def home():
 if __name__=='__main__':
 
     app.run(host='0.0.0.0',port=8080,debug=True)
+
+
