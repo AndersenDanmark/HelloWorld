392cde9bdcb7ffcd0930129eba7807daaf61cd1a
AndersenDanmark <andrewyan7217@gmail.com>
2018-05-19 11:30:43 -0600

    added mongoDB.py

--- app.py
+++ app.py
@@ -24,15 +24,24 @@ def home():
     print('Flask')
     return send_from_directory('','index.html')
 
-conn = pymysql.connect(host='s0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', port=3306, user='yln2gxt9djlnxre6', passwd='gzkozs8m6j1gfk4h', db='b59xwd91o4xf8moh')
+conn = pymysql.connect(host='s0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+                        port=3306, 
+                        user='yln2gxt9djlnxre6',
+                        passwd='gzkozs8m6j1gfk4h',
+                        db='b59xwd91o4xf8moh')
 cur = conn.cursor()
+
 sql="INSERT INTO b59xwd91o4xf8moh.HOUSES (MLS_Number,SQF,Land_Size,Price,Mag_Fees,Year_Built,Sotreys,Bedrooms,Washrooms) VALUES ('C4172305', '1896', '143','504900','0','1990','2','4','4');"
 
 cur.execute(sql)
+conn.commit()
 
 #cur.execute(print([i [0] for i in cur] [0]))
 
-
+cur.execute("SELECT * FROM b59xwd91o4xf8moh.HOUSES")
+for row in cur:
+  print(row)
+conn.close()
 
 if __name__=='__main__':
 
