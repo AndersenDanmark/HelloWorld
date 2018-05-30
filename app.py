50864b048cb9c59f8b0cdbc799707499ea51d6b1
Andrew Yan <38387309+AndersenDanmark@users.noreply.github.com>
2018-05-30 15:55:46 -0600

    updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 print("Hello World! monkeymonkey")
 
-from flask import Flask
+from flask import Flask, render_template,abort
 from flask import send_from_directory
 from flask import request
 import pymongo
@@ -9,27 +9,10 @@ import json
 import pymysql
 import json
 
-#build connection
-
-client =MongoClient("mongodb://andrewyan:andrewyan!23@ds237660.mlab.com:37660/heroku_zdtgskz7")
-
-#creating a database called jobs_database
-db = client.heroku_zdtgskz7
-
-#creating a collection (table) called collection
-collection = db.datasciencejobs
-
-cursor=collection.find({"Company Name":"eBay Inc."})
-job_descriptions={}
-for doc in cursor:
-    job_descriptions=doc
-
-print(job_descriptions)
-
 app=Flask(__name__,static_url_path='',static_folder='web/')
 
 print('Starting Flask')
-
+'''
 @app.route('/add')
 def add():
     x= int(request.args.get('x'))
@@ -37,19 +20,38 @@ def add():
     z=x+y
     ret = json.dumps(z)
     return ret
-
+'''
 @app.route('/')
 def home():
 
     print('Flask')
     return send_from_directory('','index.html')
+#build connection
 
-@app.route('/')
+client =MongoClient("mongodb://andrewyan:andrewyan!23@ds237660.mlab.com:37660/heroku_zdtgskz7")
+
+#creating a database called db
+db = client.heroku_zdtgskz7
+
+#creating a collection (table) called collection
+collection = db.datasciencejobs
+
+cursor=collection.find({"Company Name":"eBay Inc."},{"_id":0})
+job_descriptions={}
+for doc in cursor:
+    job_descriptions=doc
+print(job_descriptions)  
+
+print('123')
+@app.route('/getjob')
 def getjob():
     ret=json.dumps(job_descriptions)
-
-    print(ret)
+    print("this is the getjob function")
+    return(ret)
     
+getjob()    
+print('456')
+
 if __name__=='__main__':
 
     app.run(host='0.0.0.0',port=8080,debug=True)
