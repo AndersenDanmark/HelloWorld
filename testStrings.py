07923f2f77154c983382be232bcf90348c24375c
Andrew Yan <38387309+AndersenDanmark@users.noreply.github.com>
2018-06-04 15:06:13 -0600

    HTML5Up Uploaded

---
+++ testStrings.py
@@ -0,0 +1,31 @@
+import math
+import os
+import random
+import re
+import sys
+
+'''
+
+# Complete the reverseShuffleMerge function below.
+def reverseShuffleMerge(s):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+'''
+s =list(input())
+
+splited=list(s)
+print(splited)
+
+b=sorted(s,reverse=True)
+print(b)
+     
+'''
+    result = reverseShuffleMerge(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+'''
+
+
