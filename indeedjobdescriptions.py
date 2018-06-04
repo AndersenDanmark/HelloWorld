b8f9b6da1fc6295adefe0ac816107fd9152c8d1d
Andrew Yan <38387309+AndersenDanmark@users.noreply.github.com>
2018-06-04 15:57:35 -0600

    HTML5

---
+++ indeedjobdescriptions.py
@@ -0,0 +1,220 @@
+
+# coding: utf-8
+
+# In[130]:
+
+
+import re
+from nltk.corpus import stopwords
+from goose3 import Goose
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import *
+import time
+import requests
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+import pymongo
+from pymongo import MongoClient
+import json
+import pprint
+
+
+# In[131]:
+
+
+base_url = "http://www.indeed.com/"    
+#change the start_url can scrape different cities.
+start_url = "http://www.indeed.com/jobs?q=data+scientist&l=San+Francisco%2C+CA"
+
+#creating a response object called resp
+resp = requests.get(start_url)
+
+#You can find out what encoding Requests is using, and change it, using the r.encoding property
+#print(resp.encoding)
+
+# Running the url link through BeautifulSoup give us a BeautifulSoup object, which represents the document as a nested data structure.
+start_soup = BeautifulSoup(resp.content)
+urls = start_soup.findAll('a',{'rel':'nofollow','target':'_blank'}) #this are the links of the job posts
+
+
+# In[132]:
+
+
+urls[0]
+
+
+# In[133]:
+
+
+#To store all url links to a list called links.
+#urls=[]
+#for link in urls:
+#    urls.append(link.get('href'))
+#print(urls[0:2])
+
+
+# In[134]:
+
+
+urls = [link['href'] for link in urls] 
+
+
+# In[135]:
+
+
+# prevent the driver stopping due to the unexpectedAlertBehaviour.
+webdriver.DesiredCapabilities.FIREFOX["unexpectedAlertBehaviour"] = "accept"
+get_info = True
+
+DRIVER_EXE = r"C:\Users\Andrew Yan\Documents\GitHub\data_skills\geckodriver.exe"
+#the instance of Firefox WebDriver is created.
+
+
+# In[136]:
+
+
+driver=webdriver.Firefox(executable_path=DRIVER_EXE)
+
+
+# In[137]:
+
+
+# set a page load time limit so that don't have to wait forever if the links are broken.
+driver.set_page_load_timeout(15) 
+for i in range(len(urls)): #change it back to for i in range(len(urls)): 
+    get_info = True
+    try:
+        driver.get(base_url+urls[i]) #The driver.get method will navigate to a page given by the URL.
+    except TimeoutException:
+        get_info = False
+        continue
+    j = random.randint(1000,2200)/1000.0
+    time.sleep(j) #waits for a random time so that the website don't consider you as a bot
+    if get_info:
+        soup=BeautifulSoup(driver.page_source)
+        
+        head_tag=soup.head.title
+        
+        title_tag = head_tag.contents[0]
+        
+        print(title_tag)
+        
+        
+        #job_title=soup.b.string
+        
+        job_title=soup.find_all("b",{"class":"jobtitle"})[0]              
+        #print(job_title.text)
+        
+    
+        company=soup.find_all("span",{"class":"company"})[0]
+        #print(company.text)
+        
+        location=soup.find_all("span",{"class":"location"})[0]
+        #print(location.text)
+        
+        job_description=soup.find_all("span",{"id":"job_summary"})[0]
+       
+        #print(job_description.text)
+
+        #print (driver.current_url)
+        
+        
+        jobs={
+            
+            "Job Title":job_title.text,
+            "Company Name":company.text,
+            "Location":location.text,
+            "Url":driver.current_url,
+            "Job Description":job_description.text
+            
+        }
+
+        with open(r"C:\Users\Andrew Yan\Documents\GitHub\HelloWorld\jobs.json","w") as outfile:
+            json.dump(jobs,outfile)
+ 
+        
+        print(jobs)
+        
+driver.quit()
+    
+
+
+# In[145]:
+
+
+
+
+
+#build connection
+#client = MongoClient('mongodb://127.0.0.1:27017')
+
+client =MongoClient("mongodb://andrewyan:andrewyan!23@ds237660.mlab.com:37660/heroku_zdtgskz7")
+
+#creating a database called jobs_database
+db = client.heroku_zdtgskz7
+
+#creating a collection (table) called collection
+collection = db.datasciencejobs
+
+#job_id = db.collection.insert_one(jobs).inserted_id
+#collection.find_one({"Job Title": "Data Scientist"})
+
+
+
+def readFromMongoDB(db, collection, ID):
+  
+  ID = int(ID)
+  client =MongoClient("mongodb://andrewyan:andrewyan!23@ds237660.mlab.com:37660/heroku_zdtgskz7")
+  mydb = client[heroku_zdtgskz7]
+  qry = {"ID": ID}
+  obj = mydb[datasciencejobs].find_one(qry,{'_id': False}) # the second parameter {'_id': False} causes the objectID not to be returned
+  #print('Mongo obj:', obj)
+  client.close()
+  return obj
+'''
+{
+  "_id": "heroku_zdtgskz7.andrewyan",
+  "user": "andrewyan",
+  "db": "heroku_zdtgskz7",
+  "roles": [
+      {
+          "role": "dbOwner",
+          "db": "heroku_zdtgskz7"
+      }
+  ]
+}
+
+'''
+def writeToMongoDB(db, collection, ID, jobs):
+
+  ID = int(ID)
+  client =MongoClient("mongodb://andrewyan:andrewyan!23@ds237660.mlab.com:37660/heroku_zdtgskz7")
+  #client = MongoClient("mongodb://localhost:27017/")
+
+  # Connect to DB:
+  # mydb = client.heroku_dgltjp2d
+  mydb = client[heroku_zdtgskz7]
+
+  obj = json.loads(jobs)
+
+  doc = { "ID":ID, "value" : obj }
+  
+  
+  # if this ID doesnt exist yet, insert a new one
+  #mydb[_tableName].insert_one(doc)
+  
+  # if this ID already exists, we will need to update it with a new value
+  # mycollection.update({'_id':mongo_id}, {"$set": post}, upsert=False)
+  mydb[db].update({ 'ID':ID }, doc, upsert=True )
+  
+  client.close()
+
+  return
+
+
+
+pprint.pprint(collection.find_one())
+
---
+++ testStrings.py
@@ -0,0 +1,66 @@
+<<<<<<< HEAD
+import math
+import os
+import random
+import re
+import sys
+
+'''
+
+# Complete the reverseShuffleMerge function below.
+def reverseShuffleMerge(s):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+'''
+s =list(input())
+
+
+reversedA=sorted(s,reverse=True)
+print(b)
+
+shuffledA=
+
+     
+'''
+    result = reverseShuffleMerge(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+'''
+
+
+=======
+import math
+import os
+import random
+import re
+import sys
+
+'''
+
+# Complete the reverseShuffleMerge function below.
+def reverseShuffleMerge(s):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+'''
+s =list(input())
+
+splited=list(s)
+print(splited)
+
+b=sorted(s,reverse=True)
+print(b)
+     
+'''
+    result = reverseShuffleMerge(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+'''
+
+
+>>>>>>> 42f569604d9f1629a860800d13314643a37f75e9
