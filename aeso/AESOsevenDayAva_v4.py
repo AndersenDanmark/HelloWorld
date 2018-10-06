10e031c7397fbafc29820ef85b02a87477ccbab3
Andrew Yan <38387309+AndersenDanmark@users.noreply.github.com>
2018-10-06 02:06:47 -0600

    updated

--- aeso/AESOsevenDayAva_v4.py
+++ aeso/AESOsevenDayAva_v4.py
@@ -5,13 +5,20 @@ import re
 import datetime
 import psycopg2
 import psycopg2.extras
+
+#how to use the lib below
 from sqlalchemy import create_engine
 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#set up display
 pd.set_option('display.width',1000)
 pd.set_option('display.max_rows',100)
 pd.set_option('display.max_columns',10)
 
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#set up variables and queries
 QUERY_INSERT_MC = "INSERT INTO public.seven_day_mc (aeso_report_time, script_update_time,coal, gas, hydro, wind, other) VALUES(%s,%s,%s, %s, %s, %s, %s);"
 QUERY_INSERT_BASE = "INSERT INTO public.seven_day_{} (forecast_date, hour_ending, availability, aeso_report_time, script_update_time) VALUES(%s, %s, %s, %s, %s);"
 DB_COLUMNS = ['forecast_date', 'hour_ending', 'availability', 'aeso_report_time', 'script_update_time']
@@ -20,6 +27,9 @@ DATA_URL = "http://ets.aeso.ca/ets_web/ip/Market/Reports/SevenDaysHourlyAvailabl
 FUEL_TYPE = ['coal', 'gas', 'hydro', 'wind', 'other']
 
 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#functions below
 def formatData(data):
     '''
     The formatData() function takes in a table and return each fuel type's manufacturing capacity and the availabity in float data.
@@ -125,7 +135,10 @@ def create_fuel_type_tables(dataTable,aeso_report_time,script_update_time):
 
 
 def convert_and_Insert_To_MC(data):
-    
+    '''
+    extract values from series called generation_mc_table and put all the variables into a list.
+    Then insert these variables into the database
+    '''
     
     aeso_report_time = generation_mc_table.loc[0].at['aeso_report_time']
     script_update_time= generation_mc_table.loc[0].at['script_update_time']
@@ -146,33 +159,43 @@ def convert_and_Insert_To_MC(data):
     
     return data
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#Connect to the database
 conn = psycopg2.connect(dbname='power', user='postgres', host='149.28.238.141', password="xiaoyu03")
 cur = conn.cursor()   
 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#fetch data from AESO website
 dataTable, aeso_report_time, script_update_time = fetchData(DATA_URL)
 
 fuel_type_av_table,mc = create_fuel_type_tables(dataTable,aeso_report_time,script_update_time)
 
-#create a DataFrame from  mc
 
+
+#create a DataFrame from  mc
 generation_mc_table=pd.DataFrame(pd.Series(mc)).T
 
 
- 
+
+#insert data in to the "seven_day_mc" table in database "Power "
 convert_and_Insert_To_MC(generation_mc_table)
 
- # insert fuel type data into the database for each fuel type      
+
+
+#insert 7-day availability by fuel type data into each fuel type tables     
 aeso_avalibility_by_fuel_type = updateAllFuelTypeAv(fuel_type_av_table)
 
 
 
 #close the connection to the database
 conn.close()
-
 print("7 day fuel type data has been updated")    
     
         
-        
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+# Additional things need to study
+       
 # ====== Connection ======
 # Connecting to PostgreSQL by providing a sqlachemy engine
 #engine = create_engine('postgresql://'+os.environ['POSTGRESQL_USER']+':'+os.environ['POSTGRESQL_PASSWORD']+'@'+os.environ['POSTGRESQL_HOST_IP']+':'+os.environ['POSTGRESQL_PORT']+'/'+os.environ['POSTGRESQL_DATABASE'],echo=False)
